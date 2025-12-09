79188348fa11d3700dd62d9f877d609f38a94478
Alexia Wells <alexiarose6131@gmail.com>
2025-12-09 05:53:28 -0800

    Added icons to cards

--- Final/personal-portfolio-app--/src/pages/Home.jsx
+++ Final/personal-portfolio-app--/src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
 import { Button, Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import '../App.css'
-import Project3 from './Project 3.jsx';
+import websiteIcon from '../assets/icons/Web.svg'
+import appIcon from '../assets/icons/Application.svg'
+import databaseIcon from '../assets/icons/Database.svg'
 
 const Home = () => {
     return (
         <>
             <Container>
                 <Row className="mt-4">
-                    <Col className="col-12 col-md-6">
+                    <Col className="col-12 col-md-6 border space">
                         <h1>About Me</h1>
                                 <p>
                                     I'm a beginner programmer currently learning C#, SQL, HTML, CSS, and JavaScript. 
@@ -25,11 +26,11 @@ const Home = () => {
                     </Col>
                     <Col className="col-12 col-md-6">
                         <Container>
-                            <Row className='p-3'>
+                            <Row className='p-3 border space'>
                                 <h2>Education:</h2>
                                 <p>In Progress: SPSCC - AAS Software Devolopment</p>
                             </Row>
-                            <Row className='p-3'>
+                            <Row className='p-3 border space'>
                                 <h2>Contact info:</h2>
                                 <p>alexiarose6131@gmail.com</p>
                                 <h2>Github:</h2><a href='https://github.com/lexirose6'>lexirose6</a>
@@ -38,26 +39,29 @@ const Home = () => {
                     </Col>
                 </Row>
                 <Row>
+                    <h1 className='space'>Projects</h1>
+                </Row>
+                <Row>
                     <Col className='col-12 col-md-4'>
                         <div className='card'>
-                            <img src="null" alt=""></img>
-                            <h3>Doggy Delights</h3><br/>
+                            <img src={websiteIcon} alt="Laptop icon"></img>
+                            <h3 className='space'>Doggy Delights</h3>
                             <p>A multi-page website that showcases many menu items.</p>
                             <Button href="/Project1">Go to page</Button>
                         </div>
                     </Col>
                     <Col className='col-12 col-md-4'>
                         <div className='card'>
-                            <img src="null" alt=""></img>
-                            <h3>Car Dealership Application</h3><br/>
+                            <img src={appIcon} alt="Application icon"></img>
+                            <h3 className='space'>Car Dealership Application</h3>
                             <p>A windows application that allows users to list cars for sale.</p>
                             <Button href="/Project2">Go to page</Button>
                         </div>
                     </Col>
                     <Col className='col-12 col-md-4'>
                         <div className='card'>
-                            <img src="null" alt=""></img>
-                            <h3>Retrieving Data</h3><br/>
+                            <img src={databaseIcon} alt="Database icon"></img>
+                            <h3 className='space'>Retrieving Data</h3>
                             <p>Retrieves Invoice data with LINQ quieries and displays results onto a form.</p>
                             <Button href='/Project3'>Go to page</Button>
                         </div>
