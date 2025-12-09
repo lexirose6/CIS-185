a92560fe45a4b30d02439c19a0f1c05d7a70ff90
Alexia Wells <alexiarose6131@gmail.com>
2025-12-09 05:51:27 -0800

    Added carousel to Project1

--- Final/personal-portfolio-app--/src/pages/Project 1.jsx
+++ Final/personal-portfolio-app--/src/pages/Project 1.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Carousel} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Screenshot1 from '../assets/images/Doggy Delights Screenshot 1.jpeg';
+import Screenshot2 from '../assets/images/Doggy Delights Screenshot 2.jpeg';
+import Screenshot3 from '../assets/images/Doggy Delights Screenshot 3.jpeg';
+import BootstrapIcon from '../assets/icons/Bootstrap.svg';
+import VSCodeIcon from '../assets/icons/vscode.svg';
 
 const Project1 = () => {
     return (
         <>
             <Container>
-                <h1>Doggy Delights</h1>
+                <div className='space'>
+                    <h1>Doggy Delights</h1>
+                </div>
                 <Row>
-                    
-                    <img src="null" alt=""></img>
-                    
+                    <Carousel>
+                            <Carousel.Item>
+                                <img src={Screenshot1} className="d-block w-100 big" alt="Home page"/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img src={Screenshot2} className="d-block w-100 big" alt="Location page"/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img src={Screenshot3} className="d-block w-100 big" alt="Suggestion page"/>
+                            </Carousel.Item>
+                    </Carousel>
                 </Row>
                 <Row>
-                    <div>
-                        <h2>Description</h2><br/>
+                    <div className='space border'>
+                        <h2>Description</h2>
                         <p>Doggy Delights is a website designed for a bakery that produces dog-friendly drinks and treats. 
                             The website includes a drink and pastry menu, along with holiday specials. 
                             It also inlcudes an about page, location page, and a suggestion page. 
@@ -28,18 +42,18 @@ const Project1 = () => {
                             <li>Responsive design.</li>
                         </ul>
                     </div>
-                    <div>
-                         <h3>Link: <a href="https://github.com/lexirose6/CIS-185/tree/main/Midterm">Github: Doggy Delights</a></h3>
+                    <div className='space border'>
+                         <h3>Link: <a className="links" href="https://github.com/lexirose6/CIS-185/tree/main/Midterm">Github: Doggy Delights</a></h3>
                     </div>
-                    <div>
+                    <div className='space border'>
                         <h3>Technology Used:</h3>
                         <ul>
                             <div>
-                                <img className='icon-align' src="null" alt="Visual Studio Code icon"></img>
+                                <img className='icon-align' src={VSCodeIcon} alt="Visual Studio Code icon"></img>
                                 <p className='text-container'>Visual Studio Code</p>
                             </div>
                             <div>
-                                <img className='icon-align' src="null" alt="Bootstrap icon"></img>
+                                <img className='icon-align' src={BootstrapIcon} alt="Bootstrap icon"></img>
                                 <p className='text-container'>Bootstrap</p>
                             </div>
                         </ul>
